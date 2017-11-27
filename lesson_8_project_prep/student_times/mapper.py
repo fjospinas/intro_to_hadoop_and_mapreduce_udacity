ec2abfaff0f5a14925e06b6ed2002228cc9cb486
Francisco Ospina <francisco.ospina@gmail.com>
2017-11-27 17:55:50 -0500

    Add exercise student_times for lesson 8

---
+++ lesson_8_project_prep/student_times/mapper.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+import sys
+import csv
+from datetime import datetime as dt
+
+
+def mapper():
+    reader = csv.reader(sys.stdin, delimiter='\t')
+
+    for line in reader:
+
+        if len(line) == 19:
+
+            user, time_str = [line[3], line[8]]
+
+            if time_str != 'added_at':
+
+                if time_str.find('+') > 0:
+                    time_str = time_str[:time_str.find('.')]
+
+                time_obj = dt.strptime(time_str, '%Y-%m-%d %H:%M:%S')
+                hour = str(time_obj.hour)
+
+                print('{0}\t{1}\t1'.format(user, '0' + hour if time_obj.hour < 10 else hour))
+
+
+mapper()
---
+++ lesson_8_project_prep/student_times/reducer.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+
+import sys
+
+def get_max(dic):
+    vals = [value for key, value in dic.items()]
+    max_val = max(vals)
+    key_max = [key for key, value in dic.items() if value == max_val]
+    return key_max
+
+
+def reducer():
+
+    actual_user = None
+    dict_hours = {}
+
+    for line_act in sys.stdin:
+        line = line_act.strip().split("\t")
+
+        if actual_user and line[0] != actual_user:
+
+            key_max = get_max(dict_hours)
+            for key in key_max:
+                print('{0}\t{1}'.format(actual_user, key))
+
+            actual_user = line[0]
+            dict_hours = {}
+
+        actual_user = line[0]
+        if line[1] in dict_hours:
+            dict_hours[line[1]] = dict_hours[line[1]] + 1
+        else:
+            dict_hours[line[1]] = 1
+
+    if actual_user != None:
+        key_max = get_max(dict_hours)
+        for key in key_max:
+            print('{0}\t{1}'.format(actual_user, key))
+
+
+reducer()
